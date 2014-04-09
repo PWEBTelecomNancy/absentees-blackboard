050d5854e08fbc6e3d3cab66cc1d41754dec2b1f
Beuget Maël <mael.beuget@telecomnancy.net>
2014-04-09 17:17:42 +0200

    Une vue possible pour afficher le cour courant et les élèves par groupe, dites moi si ca va, ce qu'il y a à rajouter, retirer ...

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from HomeHandler import *
 from StudentsListHandler import *
+from CourHandler import *
 
 app = webapp2.WSGIApplication([
                                   ('/', HomeHandler),
-                                  ('/students_list', StudentsListHandler)
+                                  ('/students_list', StudentsListHandler),
+                                  ('/cour',CourHandler)
                               ], debug=True)
