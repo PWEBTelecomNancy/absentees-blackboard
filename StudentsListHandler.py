39810c88d6be471c4fca0dc3fff6361544d017cf
Videl Smith <videl@fslhome.org>
2014-04-09 21:46:11 +0200

    Adds: List of members of a group.
    With working caches and templates.

--- StudentsListHandler.py
+++ StudentsListHandler.py
@@ -1,6 +1,9 @@
 __author__ = 'Mael Beuget, Pierre Monnin & Thibaut Smith'
 
 from BaseHandler import *
+import logging
+from XMLAnalyser import XMLAnalyser
+from google.appengine.api import memcache
 
 class StudentsListHandler(BaseHandler):
     def __init__(self, request=None, response=None):
@@ -10,3 +13,27 @@ class StudentsListHandler(BaseHandler):
 
     def get(self):
         self.render("groupchoice.html")
+
+    def post(self):
+        group_to_find = self.request.get("group_name")
+
+
+        groups = memcache.get("group_list")
+
+        if groups is None:
+            logging.error("CACHE MISS StudentsListHandler l. 24")
+            parser = XMLAnalyser()
+            groups = parser.get_members()
+            memcache.set("group_list", groups)
+
+        to_display = dict()
+        for key in groups:
+            if group_to_find in key:
+                to_display[key] = groups[key]
+
+        logging.error(to_display)
+
+        if len(to_display) > 0:
+            self.render("groupdisplay.html", group_name = group_to_find, groups = to_display)
+        else:
+            self.render("message.html", title = "No such group", subtitle = "", argument = group_to_find)
--- XMLAnalyser.py
+++ XMLAnalyser.py
@@ -1,7 +1,8 @@
-
+# -*- coding: latin-1 -*-
 from xml.dom import minidom
 import urllib2
 import logging
+import codecs
 
 
 class XMLAnalyser():
@@ -39,14 +40,50 @@ class XMLAnalyser():
 
     def get_xml(self, url):
         page = urllib2.urlopen(url)
-        return minidom.parseString(page.read())
+        logging.error(url)
+        content = page.read()
 
-    def query(self, ):
+        return minidom.parseString(content)
+
+    def query(self, parameters):
         if self.session_id is "":
-            self.session_id = XMLAnalyser.get_session_id()
+            self.session_id = self.get_session_id()
 
         if self.projectId is "":
-            self.projectId = XMLAnalyser.get_project_id()
+            self.projectId = self.get_project_id()
 
         if not self.projectSet:
             self.set_project_id()
+
+        parameters["sessionId"] = self.session_id
+
+        query = self.craft_url(parameters)
+        logging.error(query)
+        xml = self.get_xml(query)
+
+        return xml
+
+    def get_members(self):
+        clubs_list = dict()
+
+        x = self.query({"function": "getResources", "detail": "13", "tree": "false"})
+        resources = x.getElementsByTagName("resource")
+
+        for x in range(0, resources.length):
+            item = resources[x]
+            if item.getAttribute("category") == "category5": # category5 <- most students
+                groups = item.getElementsByTagName("memberships")
+                groups = groups[0].childNodes
+
+
+                # print groups.toprettyxml()
+                for y in range(0, groups.length):
+                    group = groups[y]
+
+                    if group.nodeName == "membership":
+                        if group.getAttribute("name") not in clubs_list :
+                            clubs_list[group.getAttribute("name")] = list()
+
+                        clubs_list[group.getAttribute("name")].append(item.getAttribute("name"))
+
+        return clubs_list
