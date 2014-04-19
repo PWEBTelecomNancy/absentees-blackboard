ffe2225ea2558198775b2d4f6490ee59e06e8fe2
Pierre Monnin <pier.monn@gmail.com>
2014-04-19 21:01:35 +0200

    HTML/CSS ready for login and signin

--- LoginHandler.py
+++ LoginHandler.py
@@ -10,3 +10,21 @@ class LoginHandler(BaseHandler):
 
     def get(self):
         self.render('login.html')
+
+    def post(self):
+        username_present = False
+        password_present = False
+
+        if self.request.post('login'):
+            username_present = True
+
+        if self.request.post('password'):
+            password_present = True
+
+        if not username_present or not password_present:
+            pass
+            # We display again the same form with error message
+
+        else:
+            pass
+            # It's okay we can add it to the base and connect the user
--- SignupHandler.py
+++ SignupHandler.py
@@ -10,3 +10,25 @@ class SignupHandler(BaseHandler):
 
     def get(self):
         self.render('signup.html')
+
+    def post(self):
+        username_present = False
+        password_present = False
+        email_present = True
+
+        if self.request.post('login'):
+            username_present = True
+
+        if self.request.post('password'):
+            password_present = True
+
+        if self.request.post('email'):
+            email_present = True
+
+        if not username_present or not password_present:
+            pass
+            # We display again the same form with error message
+
+        else:
+            pass
+            # It's okay we can add it to the base and connect the user
