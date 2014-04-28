b4671f35e8c3a363484a534dabc201621c75dffa
Pierre Monnin <pier.monn@gmail.com>
2014-04-28 19:30:54 +0200

    Stories one and two work

--- handler/ClassAbsenteesHandler.py
+++ handler/ClassAbsenteesHandler.py
@@ -3,7 +3,8 @@ import re
 
 from handler.BaseHandler import *
 from model.ADECommunicator import *
-
+from model.Absentees import *
+import time
 
 class ClassAbsenteesHandler(BaseHandler):
     temp_prof_name = "CHAROY FRANCOIS"
@@ -15,7 +16,7 @@ class ClassAbsenteesHandler(BaseHandler):
         self.ade_communicator = ADECommunicator()
 
     def filter_teacher_class(self, teacher, time, date):
-        all_lessons = self.ade_communicator.get_lessons()
+        """all_lessons = self.ade_communicator.get_lessons()
 
         teacher_classes = list()
 
@@ -71,12 +72,12 @@ class ClassAbsenteesHandler(BaseHandler):
                     final_step = lesson
             # else if hours are not the same, compare hours
             elif lesson_start_time[0] < my_time[0] and my_time[0] < lesson_end_time[0]:
-                final_step = lesson
+                final_step = lesson"""
 
-        #return {"class_name": "TP PGWEB 2A IL", "groups": ["2A IL", "2A TRS"], "start_time": "10h00", "end_time": "12h00",
-        #        "teacher_name": "CHAROY FRANCOIS", "room": "S2.42"}
+        return {"class_name": "TP PGWEB 2A IL", "groups": ["2A IL"], "start_time": "10h00", "end_time": "12h00",
+                "teacher_name": "CHAROY FRANCOIS", "room": "S2.42"}
 
-        if final_step is None:
+        """if final_step is None:
             return None
         else:
             return {"class_name": final_step["subject"],
@@ -84,19 +85,21 @@ class ClassAbsenteesHandler(BaseHandler):
                     "start_time": final_step["startHour"],
                     "end_time": final_step["endHour"],
                     "teacher_name": final_step["instructor"],
-                    "room": final_step["classroom"]}
+                    "room": final_step["classroom"]}"""
 
     def get(self):
         #Test user connexion and privileges
         if self.is_connected() and get_is_teacher_from_id(self.request.cookies.get('user_id').split('|')[0]):
             #First, get the class the teacher should have right now
-            class_to_display = self.filter_teacher_class(self.temp_prof_name,
-                                                         time.strftime("%H:%M"),
-                                                         time.strftime("%d/%m/%Y"))
-            #Then, we get the students for this class
-            students_list = []
-            groups = self.ade_communicator.get_students_groups()
+            class_date = time.strftime("%d/%m/%Y")
+            class_to_display = self.filter_teacher_class(self.temp_prof_name, time.strftime("%H:%M"), class_date)
+
+            #If there's a class
             if class_to_display is not None:
+                #Then, we get the students for this class
+                students_list = []
+                groups = self.ade_communicator.get_students_groups()
+
                 for group_name in groups:
                     for group_to_find in class_to_display['groups']:
                         if re.match(group_to_find, group_name) is not None:
@@ -108,8 +111,22 @@ class ClassAbsenteesHandler(BaseHandler):
 
                 students_list.sort(key=lambda x: x['name'])
 
+                #Check for already done absentees
+                present_absentees = get_absentees_for_class(class_to_display['class_name'],
+                                                            class_to_display['teacher_name'], class_date,
+                                                            class_to_display['start_time'],
+                                                            class_to_display['end_time'])
+
+                mail_absentees = []
+                if len(present_absentees) != 0:
+                    for student_abs in present_absentees:
+                        mail_absentees.append(student_abs.student_email)
+
                 #Render the page
-                self.render("class_absentees.html", students=students_list, **class_to_display)
+                self.render("class_absentees.html", students=students_list, absentees=mail_absentees,
+                            **class_to_display)
+
+            #Else, congrats, the teacher doesn't have to do anything
             else:
                 self.render("message.html", title="No lessons found!", subtitle="Looks like you don't have to work!")
         else:
@@ -117,4 +134,57 @@ class ClassAbsenteesHandler(BaseHandler):
                         text="It seems you're not a teacher nor a connected user")
 
     def post(self):
-        pass
+        #Test user connexion and privileges
+        if self.is_connected() and get_is_teacher_from_id(self.request.cookies.get('user_id').split('|')[0]):
+            #First, get the class the teacher should have right now
+            class_date = time.strftime("%d/%m/%Y")
+            class_to_display = self.filter_teacher_class(self.temp_prof_name, time.strftime("%H:%M"), class_date)
+
+            #If there's a class
+            if class_to_display is not None:
+                #Then, we get the students for this class
+                students_list = []
+                groups = self.ade_communicator.get_students_groups()
+
+                for group_name in groups:
+                    for group_to_find in class_to_display['groups']:
+                        if re.match(group_to_find, group_name) is not None:
+                            temp = []
+                            for student in groups[group_name]:
+                                temp.append({"name": student, "group": group_name})
+
+                            students_list.extend(temp)
+
+                students_list.sort(key=lambda x: x['name'])
+
+                #Purge current absentees
+                present_absentees = get_absentees_for_class(class_to_display['class_name'],
+                                                            class_to_display['teacher_name'], class_date,
+                                                            class_to_display['start_time'],
+                                                            class_to_display['end_time'])
+                db.delete(present_absentees)
+
+                #Check who is present now from post argument
+                for student in students_list:
+                    name = student['name']['name']
+                    mail = student['name']['mail']
+
+                    if self.request.get(mail + "|" + name + "|box"):
+                        absentee = Absentees(student_name=name, student_email=mail,
+                                             student_group=student['group'],
+                                             class_title=class_to_display['class_name'],
+                                             teacher_name=class_to_display['teacher_name'],
+                                             start_hour=class_to_display['start_time'],
+                                             end_hour=class_to_display['end_time'],
+                                             class_date=class_date, justification_bool=False)
+                        absentee.put()
+
+                time.sleep(1)
+                self.redirect('/class_absentees')
+            #Else, congrats, the teacher doesn't have to do anything
+            else:
+                self.redirect('/class_absentees')
+
+        else:
+            self.render("message.html", title="Access fobidden",
+                        text="It seems you're not a teacher nor a connected user")
--- model/Absentees.py
+++ model/Absentees.py
@@ -8,10 +8,18 @@ class Absentees(db.Model):
     student_email = db.StringProperty(required=True)
     student_group = db.StringProperty(required=True)
     class_title = db.StringProperty(required=True)
-    start_hour = db.TimeProperty(required=True)
-    end_hour = db.TimeProperty(required=True)
-    class_date = db.DateProperty(required=True)
+    teacher_name = db.StringProperty(required=True)
+    start_hour = db.StringProperty(required=True)
+    end_hour = db.StringProperty(required=True)
+    class_date = db.StringProperty(required=True)
     justification_bool = db.BooleanProperty(required=True)
     justification_text = db.StringProperty
 
 
+def get_absentees_for_class(class_title, teacher_name, class_date, start_hour, end_hour):
+    result = db.GqlQuery("SELECT * FROM Absentees WHERE class_title=:class_title AND teacher_name=:teacher_name AND "
+                         "class_date=:class_date AND start_hour=:start_hour AND end_hour=:end_hour",
+                         class_title=class_title, teacher_name=teacher_name, class_date=class_date,
+                         start_hour=start_hour, end_hour=end_hour)
+    result = list(result)
+    return result
