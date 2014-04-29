225fc135b9ccab43820ca0bdd4fedf44e1b259f3
Pierre Monnin <pier.monn@gmail.com>
2014-04-29 11:45:57 +0200

    Administration pages for logs and parameters

---
+++ handler/ParametersHandler.py
@@ -0,0 +1,12 @@
+__author__ = 'Mael Beuget, Pierre Monnin & Thibaut Smith'
+
+from handler.BaseHandler import *
+
+
+class ParametersHandler(BaseHandler):
+    def __init__(self, response=None, request=None):
+        self.initialize(response, request)
+        self.page_name = "administration"
+
+    def get(self):
+        self.response.out.write("Parameters handler to be coded")
