f4074015e37a0f4cb240e4acb212f839874be8a4
Pierre Monnin <pier.monn@gmail.com>
2014-04-29 17:35:49 +0200

    Members administration page (HTML stuff only)

--- handler/MembersAdminHandler.py
+++ handler/MembersAdminHandler.py
@@ -1,6 +1,7 @@
 __author__ = 'Mael Beuget, Pierre Monnin & Thibaut Smith'
 
 from handler.BaseHandler import *
+from model.Accounts import *
 
 
 class MembersAdminHandler(BaseHandler):
@@ -9,4 +10,12 @@ class MembersAdminHandler(BaseHandler):
         self.page_name = "administration"
 
     def get(self):
-        self.response.out.write("To be coded")
+        if self.is_connected() and get_is_admin_from_id(self.request.cookies.get('user_id').split('|')[0]):
+            query = Accounts.all()
+            accounts = query.fetch(limit=None)
+
+            self.render("administration_members.html", accounts=accounts)
+
+        else:
+            self.render("message.html", title="Access forbidden",
+                        text="It seems you're not an administrator nor a connected user")
