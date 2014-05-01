b959085cde0c4e91982230880288636e37c7a391
Videl Smith <videl@fslhome.org>
2014-05-01 09:00:21 +0200

    Adds: error message on unauthaurized access.

--- handler/LogsHandler.py
+++ handler/LogsHandler.py
@@ -10,13 +10,26 @@ class LogsHandler(BaseHandler):
         self.page_name = "administration"
 
     def get(self):
-        all_logs = Logs.query().order(-Logs.date_time).fetch()
+        # If the user isn't connected nor isn't administrator => error message
+        if not (self.is_connected() and
+                    get_is_admin_from_id(
+                            self.request.cookies.get('user_id').split('|')[0])):
+            self.error(404)
+            self.render("message.html", title="Access forbidden",
+                        text="It seems you're not an administrator nor a connected user.")
 
+        else:
+            all_logs = Logs.query().order(-Logs.date_time).fetch()
 
-        self.render("administration_logs.html", all_logs=all_logs)
+            self.render("administration_logs.html", all_logs=all_logs)
 
     def post(self):
-        all_logs = Logs.query().order(-Logs.date_time).fetch()
+        # If the user isn't connected nor isn't administrator => error message
+        if not (self.is_connected() and get_is_admin_from_id(self.request.cookies.get('user_id').split('|')[0])):
+            self.render("message.html", title="Access forbidden",
+                        text="It seems you're not an administrator nor a connected user.")
 
+        else:
+            all_logs = Logs.query().order(-Logs.date_time).fetch()
 
-        self.write("To be coded.")
+            self.write("To be coded.")
